3f7b3f1c755a642648cbd4c7583f1e9d4e1b6ae1
MouraRei <reinaldomoura.contato@outlook.com>
2021-11-19 10:50:42 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿ContaCorrente contaDaGabriela = new ContaCorrente();
+
+contaDaGabriela.titular = "Gabriela";
+contaDaGabriela.agencia = 481;
+contaDaGabriela.conta = 451726;
+contaDaGabriela.saldo = 240.30;
+
+Console.WriteLine(contaDaGabriela.titular);
+Console.WriteLine("Agencia: "+ contaDaGabriela.agencia);
+Console.WriteLine("Conta: " +contaDaGabriela.conta);
+Console.WriteLine("Saldo: "+ contaDaGabriela.saldo);
+
+
+Console.ReadLine();
